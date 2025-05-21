1495ebf2f695614e0e7f8c4e79c01ec43c8896ab
Monika <85540663+Monika135@users.noreply.github.com>
2025-05-21 19:36:00 +0530

    Remove the commented code

--- app.py
+++ app.py
@@ -1,32 +1,5 @@
 
 
-# # @app.route("/webhook", methods=["POST"])
-# # def payment_webhook():
-# #     payload = request.get_json(force=True)
-
-# #     if not payload:
-# #         abort(400, "Invalid JSON")
-
-# #     try:
-# #         validate_payload(payload)
-# #     except ValueError as ve:
-# #         return jsonify({"error": str(ve)}), 400
-
-# #     # Add timestamp
-# #     payload["received_at"] = datetime.utcnow().isoformat()
-
-# #     log_event(payload)
-# #     print(
-# #         f"✅ Received: {payload['event_type']} | ID: "
-# #         f"{payload.get('transaction_id')}")
-
-# #     return jsonify({"status": "received"}), 200
-
-
-# if __name__ == "__main__":
-#     app.run(port=5002, debug=True)
-
-
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
